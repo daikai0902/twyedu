d8fd7298964fe8dfbf83003d14de7a48cfcceab0
daikai0902 <1040526401@qq.com>
2018-03-14 15:27:59 +0800

    开发

---
+++ app/controllers/Application.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import cn.bran.play.JapidController;
+
+
+
+public class Application extends JapidController {
+
+    public static void index() {
+        renderHtml("start.....");
+    }
+
+}

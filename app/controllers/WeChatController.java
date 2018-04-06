41df0827667d16ede4d722d118bba1114a6dab4a
戴凯 <kai.dai@iclass.com>
2018-04-06 22:26:25 +0800

    开发

--- app/controllers/WeChatController.java
+++ app/controllers/WeChatController.java
@@ -85,7 +85,7 @@ public class WeChatController extends BaseController{
         CourseStudent cs = CourseStudent.findById(courseStudentId);
         if(cs != null){
             cs.genOrderNum();
-            OrderResult orderResult = OrderResult.unifeOrder(cs.course.name,Integer.valueOf(cs.course.fee)*100,cs.orderNum,currentUrl,"JSAPI",openId);
+            OrderResult orderResult = OrderResult.unifeOrder(cs.course.name,(int)(Double.valueOf(cs.course.fee)*100),cs.orderNum,currentUrl,"JSAPI",openId);
             renderJSON(Result.succeed(orderResult));
         }
     }

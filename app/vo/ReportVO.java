d1e620614622f02432dc8d416d4279ca132f6002
戴凯 <kai.dai@iclass.com>
2018-04-21 04:18:59 +0800

    开发

--- app/vo/ReportVO.java
+++ app/vo/ReportVO.java
@@ -55,7 +55,7 @@ public class ReportVO extends OneData {
 
 
     public ReportVO(Report report,boolean ismobile){
-        this.id = report.id;
+        //this.id = report.id;
         this.studentName = report.student.name;
         this.clazzName  = ClazzStudent.findByStudent(report.student.id).clazz.name;
         this.studentNum = report.student.number;

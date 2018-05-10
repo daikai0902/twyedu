4a88682a54630d4887c567c9b842e4bc19a16985
戴凯 <kai.dai@iclass.com>
2018-05-10 22:05:08 +0800

    开发

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,6 +7,7 @@ import cn.bran.play.JapidController;
 public class Application extends JapidController {
 
     public static void index() {
+        System.err.println(11);
         renderHtml("start.....");
     }
 
